95581d5246d4af77a3bf63f4f08db06ba6eca222
Randy Jou <randyjou@Randys-MacBook-Pro.local>
2016-06-04 17:39:06 -0400

    We cannot all be masters, nor all masters cannot be truly follow'd

--- create_board.sh
+++ create_board.sh
@@ -1,2 +1,13 @@
+#################
+# Usage
+# create_board.sh <Name of Board>
+#
+#################
 source token
-curl -X POST -H "Content-Type: application/json" "https://trello.com/1/boards?key=$KEY&token=$TOKEN" -d '{  "name":"RELEASE X" }'
+
+regex='"shortUrl":"https://trello.com/b/([a-z0-9A-Z]*)"'
+RESPONSE=$(curl -X POST -H "Content-Type: application/json" "https://trello.com/1/boards?key=$KEY&token=$TOKEN" -d '{  "name":"'$1'" }')
+
+if [[ $RESPONSE =~ $regex ]]; then
+	echo BOARDID="${BASH_REMATCH[1]}"
+fi
