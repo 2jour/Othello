89dc79e6850d889bc2bf365429f5b7859d0d3a23
2jour <randyjou@Randys-MacBook-Pro.local>
2016-06-04 20:51:09 -0400

    Men in rage strike those that wish them best.

--- create_board.sh
+++ create_board.sh
@@ -3,7 +3,16 @@
 # create_board.sh <Name of Board> <Theme>
 #
 #################
-
+MATCH=" "
+function FetchFromResponse {
+	if [[ $1 =~ $2 ]]; then
+		echo MATCH="${BASH_REMATCH[1]}"
+		MATCH=${BASH_REMATCH[1]}
+	else
+		echo Error can not find match: Trello returned the following: "$1"
+		exit
+	fi
+}
 if [ ! -d  themes/$2 ]; then
 	echo Sorry, theme $2 not available for Othello.
 	exit
@@ -13,17 +22,15 @@ source token
 source themes/$2/config_board
 
 regex='"shortUrl":"https://trello.com/b/([a-z0-9A-Z]*)"'
+regex2='"id":"([a-z0-9A-Z]*)"'
 
-RESPONSE=$(curl -X POST -H "Content-Type: application/json" "https://trello.com/1/boards?key=$KEY&token=$TOKEN" -d '{  "name":"'$1'" }')
+RESPONSE=$(curl -X POST -H "Content-Type: application/json" "https://trello.com/1/boards?key=$KEY&token=$TOKEN" -d '{  "name":"'$1'","defaultLists":false }')
 
-BOARDID=''
-if [[ $RESPONSE =~ $regex ]]; then
-	echo BOARDID="${BASH_REMATCH[1]}"
-	BOARDID=${BASH_REMATCH[1]}
-else
-	echo Error Creating Board: Trello returned the following: "$RESPONSE"
-	exit
-fi
+BOARDIDURL=''
+
+FetchFromResponse $RESPONSE $regex 
+BOARDIDURL=$MATCH
+
 
 for x in $LABELS 
 do
@@ -31,7 +38,16 @@ do
 	LABELTAG=$(echo $x | cut -f2 -d':')
 	echo "------------------------"
 	echo Creating Label Color "$LABELCOLOR":"$LABELTAG"
-	RESPONSE=$(curl -X PUT -H "Content-Type: application/json" "https://trello.com/1/boards/$BOARDID/labelNames/$LABELCOLOR?key=$KEY&token=$TOKEN" -d '{  "value":"'$LABELTAG'" }')
-	echo "$RESPONSE"
+	RESPONSE=$(curl -X PUT -H "Content-Type: application/json" "https://trello.com/1/boards/$BOARDIDURL/labelNames/$LABELCOLOR?key=$KEY&token=$TOKEN" -d '{  "value":"'$LABELTAG'" }')
 	echo "------------------------"
+done
+
+
+FetchFromResponse $RESPONSE $regex2 
+BOARDID=$MATCH
+
+for x in $LISTS
+do
+	RESPONSE=$(curl -X POST -H "Content-Type: application/json" "https://trello.com/1/lists?key=$KEY&token=$TOKEN" -d '{  "name":"'$x'","idBoard":"'$BOARDID'" }')
+	echo $RESPONSE
 done
