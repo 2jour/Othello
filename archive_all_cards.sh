144413adec20a32b3349200368bc95a6b008ab9c
Randy Jou <randy@scratch.mit.edu>
2017-02-21 11:20:38 -0500

    Adding archive_all_cards functionality

---
+++ archive_all_cards.sh
@@ -0,0 +1,90 @@
+#######
+# NAME:
+#       archive_all_cards.sh -b <Release Name> -l <list name>
+#
+######
+set -e
+
+function fn_arch_card_help_echo {
+        clear
+        echo "------------------------------------------"
+        echo "NAME:"
+        echo "      archive_all_cards.sh -b \<Release Name\> -l <list name>"
+        echo ""
+        echo "DESCRIPTION:"
+        echo "      Archives all cards cards for a given list"
+        echo ""
+        echo "      -h - Help"
+        echo ""
+        echo "      -l - List Name where cards are to be archived"
+        echo ""
+        echo "      -b - Release Name "
+        echo
+        echo "              if there is no folder that matches <Release Name> , then error"
+        echo
+        echo "------------------------------------------"
+
+}
+
+
+arch_card_BOARD_NAME=
+arch_card_LIST_NAME=
+
+if [[ -z $@ ]]; then
+        fn_arch_card_help_echo
+        exit 1
+fi
+
+while [[ $# -gt 0 ]]; do
+        parm="$1"
+        case $parm in
+                -h)
+                fn_arch_card_help_echo
+                exit 0
+                shift
+                ;;
+                -b)
+                arch_card_BOARD_NAME="$2"
+                shift
+                ;;
+                -l)
+                arch_card_LIST_NAME="$2"
+                shift
+                ;;
+                *)
+                fn_arch_card_help_echo
+                ;;
+esac
+shift
+done
+
+if [[ -z $arch_card_BOARD_NAME ]] || [[ -z $arch_card_LIST_NAME ]] ; then
+	fn_arch_card_help_echo
+	exit 1
+fi
+
+# --- FETCHING BOARD ID
+
+if [ ! -f boards/$arch_card_BOARD_NAME/lists ] ; then
+        echo "-E- boards/$arch_card_BOARD_NAME/lists does not exist"
+        exit 1
+fi
+
+
+source boards/$arch_card_BOARD_NAME/lists
+source token
+source common_functions
+
+echo "-I- Fetching List Id"
+echo 
+arch_card_LISTID=$(fn_common_fns_fetch_list_id $arch_card_LIST_NAME "$LISTID") 
+
+
+echo "-I- Archiving cards for LIST $arch_card_LIST_NAME : $arch_card_LISTID"
+echo 
+
+arch_card_RESPONSE=$(curl -i  -X POST -H "Content-Type: application/json" "https://trello.com/1/lists/"$arch_card_LISTID"/archiveAllCards?key=$KEY&token=$TOKEN")
+
+echo "-I- Response from Trello:"
+echo
+echo $arch_card_RESPONSE
